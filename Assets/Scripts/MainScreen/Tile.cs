6a8ac7a497f04b4d200f557f05e094434196b447
Taha Murtaza Ali <ta03792@st.habib.edu.pk>
2019-04-02 15:41:52 +0500

    Grass

---
+++ Assets/Scripts/MainScreen/Tile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+    private bool occupied;
+
+    public bool Occupied { get => occupied; set => occupied = value; }
+
+}
