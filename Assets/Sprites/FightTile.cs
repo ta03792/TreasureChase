71897d092e043bf06c87a57923b4d83ee4489840
Zoha <ma02277@st.habib.edu.pk>
2019-04-30 16:38:53 +0500

    FightScreen

---
+++ Assets/Sprites/FightTile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fightile : MonoBehaviour
+{
+    private bool occupied;  // Use this for initialization  
+    void Start()
+    {
+
+    }
+    void Update() // Update is called once per frame 
+    {
+
+    }
+
+    public bool Occupied
+    {
+        get
+        {
+            return occupied;
+        }
+        set
+        {
+            occupied = value;
+        }
+    }
+}       
