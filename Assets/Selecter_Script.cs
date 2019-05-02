2f3c784922823067e1bf88e9c647c0ff8f9817b9
Mehak Irshad <mi02260@st.habib.edu.pk>
2019-05-02 11:21:26 +0500

    CharacterSelection

---
+++ Assets/Selecter_Script.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selecter_Script : MonoBehaviour
+{
+    public GameObject Wizard;
+    public GameObject Ninja;
+    public GameObject Barbarian;
+    public GameObject Pirate;
+    private Vector3 CharacterPosition;
+    private Vector3 OffScreen;
+    private int CharacterInt = 1;
+    private SpriteRenderer WizardRender, NinjaRender, BarbarianRender, PirateRender;
+    //private List<GameObject> myChars;
+    //private int SelectionIndex = 0;
+
+    //private void Start()
+    //{
+    //    myChars = new List<GameObject>();
+    //    foreach (Transform t in transform)
+    //    {
+    //        myChars.Add(t.gameObject);
+    //        t.gameObject.SetActive = false;
+
+    //    }
+    //    SelectionIndex.SetActive = true;
+            
+            
+
+    //}
+    private void Awake()
+    {
+        CharacterPosition = Wizard.transform.position;
+        OffScreen = Pirate.transform.position;
+        WizardRender = Wizard.GetComponent<SpriteRenderer>();
+        NinjaRender = Wizard.GetComponent<SpriteRenderer>();
+        BarbarianRender = Wizard.GetComponent<SpriteRenderer>();
+        PirateRender = Wizard.GetComponent<SpriteRenderer>();
+
+    }
+    public void NextCharacter()
+    {
+        switch (CharacterInt)
+        {
+            case 1:
+                WizardRender.enabled = false;
+                Wizard.transform.position = OffScreen;
+                Ninja.transform.position = CharacterPosition;
+                NinjaRender.enabled = true;
+                CharacterInt++;
+                break;
+            case 2:
+                NinjaRender.enabled = false;
+                Ninja.transform.position = OffScreen;
+                Barbarian.transform.position = CharacterPosition;
+                BarbarianRender.enabled = true;
+                CharacterInt++;
+                break;
+            case 3:
+                BarbarianRender.enabled = false;
+                Barbarian.transform.position = OffScreen;
+                Pirate.transform.position = CharacterPosition;
+                PirateRender.enabled = true;
+                CharacterInt++;
+                break;
+            case 4:
+                PirateRender.enabled = false;
+                Pirate.transform.position = OffScreen;
+                Wizard.transform.position = CharacterPosition;
+                WizardRender.enabled = true;
+                CharacterInt++;
+                ResetInt();
+                break;
+            default:
+                ResetInt();
+                break;
+        }
+    }
+    public void PreviousCharacter()
+    {
+        switch (CharacterInt)
+        {
+            case 1:
+                WizardRender.enabled = false;
+                Wizard.transform.position = OffScreen;
+                Pirate.transform.position = CharacterPosition;
+                PirateRender.enabled = true;
+                CharacterInt--;
+                ResetInt();
+                break;
+            case 2:
+                NinjaRender.enabled = false;
+                Ninja.transform.position = OffScreen;
+                Wizard.transform.position = CharacterPosition;
+                WizardRender.enabled = true;
+                CharacterInt--;
+               
+                break;
+            case 3:
+                BarbarianRender.enabled = false;
+                Barbarian.transform.position = OffScreen;
+                Ninja.transform.position = CharacterPosition;
+                NinjaRender.enabled = true;
+                CharacterInt--;
+                break;
+            case 4:
+                PirateRender.enabled = false;
+                Pirate.transform.position = OffScreen;
+                Barbarian.transform.position = CharacterPosition;
+                BarbarianRender.enabled = true;
+                CharacterInt--;
+                
+                break;
+            default:
+                ResetInt();
+                break;
+        }
+    }
+    private void ResetInt()
+    {
+        if (CharacterInt>=4) {
+            CharacterInt = 1;
+        }
+        else{
+            CharacterInt = 4;
+        }
+    }
+    public void CharacterSelect()
+    {
+       
+    }
+
+}
